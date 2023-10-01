584e0bc71adaee941d43b701d9e50a9bf5be1d55
SUDE NUR OPAN <114245040+opan12@users.noreply.github.com>
2023-10-01 16:43:58 +0300

    Add files via upload

---
+++ Freelancer/Freelancer/Program.cs
@@ -0,0 +1,34 @@
+﻿using Freelancer.Entities;
+using Freelancer.Services;
+using Freelancer.Common;
+using Freelancer.Abstract;
+
+
+
+var anotherCustomerInstance = new Customer
+{
+    Id = Guid.NewGuid(),
+    CreatedOn = DateTimeOffset.Now,
+    FirstName = "sude",
+    LastName = "opan",
+    PhoneNumber = "553-723-0946"
+};
+
+
+
+
+
+NotepadService notepadService = new();
+
+string customerData = notepadService.GetOnNotepad("C:\\Users\\sudeo\\source\\repos\\Freelancer\\Freelancer\\Database\\Costumer.txt");
+
+string[] splittedLines = customerData.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+List<Customer> customers = new();
+
+foreach (var line in splittedLines)
+{
+    Customer customer = new();
+    customer.SetValuesCSV(line);
+    customers.Add(customer);
+}
